df5e46a2e692bf8dbd35f071571ef62ed490f880
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2023-09-29 13:34:03 +0300

    Use small partition in examples

--- affinity/job_affinity_bad.sh
+++ affinity/job_affinity_bad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=affinity
 #SBATCH --account=project_465000539
-#SBATCH --partition=debug
+#SBATCH --partition=small
 #SBATCH --nodes=1               
 #SBATCH --ntasks-per-node=32     
 #SBATCH --cpus-per-task=4
--- scalability/nbody/job_nbody_smt.sh
+++ scalability/nbody/job_nbody_smt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=nbody
 #SBATCH --account=project_465000539
-#SBATCH --partition=debug
+#SBATCH --partition=small
 #SBATCH --nodes=2               
 #SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=16
